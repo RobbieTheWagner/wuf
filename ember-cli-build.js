4e61e9b865f54c18376ef5e31568497d73ef91eb
Robert Wagner <rwwagner90@gmail.com>
2020-03-03 21:01:49 -0500

    Add tailwind

--- ember-cli-build.js
+++ ember-cli-build.js
@@ -4,7 +4,25 @@ const EmberApp = require('ember-cli/lib/broccoli/ember-app');
 
 module.exports = function(defaults) {
   let app = new EmberApp(defaults, {
-    // Add options here
+    babel: {
+      plugins: ['@babel/plugin-proposal-object-rest-spread']
+    },
+    postcssOptions: {
+      compile: {
+        extension: 'scss',
+        syntax: 'postcss-scss',
+        plugins: [
+          {
+            module: require('postcss-import'),
+            options: {
+              path: ['node_modules']
+            }
+          },
+          require('tailwindcss')('./tailwind.config.js'),
+          require('postcss-nested')
+        ]
+      }
+    }
   });
 
   // Use `app.import` to add additional libraries to the generated
