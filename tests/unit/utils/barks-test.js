e4d9d537ca7384d20e6bf54a0a6a4725f8131363
Robert Wagner <rwwagner90@gmail.com>
2020-03-14 11:38:48 -0400

    Fix tests and lint

--- tests/unit/utils/barks-test.js
+++ tests/unit/utils/barks-test.js
@@ -1,11 +1,31 @@
-import barks from 'wuf/utils/barks';
-import { module, test } from 'qunit';
+import {
+  determineBarkOccurred,
+  determineBarkPitch,
+  determineBarkType,
+  getTimeDomainMaxMin
+} from 'wuf/utils/barks';
+import { module, skip, test } from 'qunit';
 
 module('Unit | Utility | barks', function() {
-
-  // Replace this with your real tests.
-  test('it works', function(assert) {
-    let result = barks();
+  skip('determineBarkOccurred', function(assert) {
+    let result = determineBarkOccurred();
     assert.ok(result);
+  });
+
+  skip('determineBarkPitch', function(assert) {
+    let result = determineBarkPitch();
+    assert.ok(result);
+  });
+
+  skip('determineBarkType', function(assert) {
+    let result = determineBarkType();
+    assert.ok(result);
+  });
+
+  test('getTimeDomainMaxMin', function(assert) {
+    const timeDomainValues = [128, 190, 90, 111, 175];
+    let { minValue, maxValue } = getTimeDomainMaxMin(timeDomainValues);
+    assert.equal(minValue, 0.3515625);
+    assert.equal(maxValue, 0.7421875);
   });
 });
--- tests/unit/utils/statistics-test.js
+++ tests/unit/utils/statistics-test.js
@@ -1,11 +1,16 @@
-import statistics from 'wuf/utils/statistics';
+import { mean, modeString } from 'wuf/utils/statistics';
 import { module, test } from 'qunit';
 
 module('Unit | Utility | statistics', function() {
+  test('mean', function(assert) {
+    const numbers = [1, 2, 3, 4, 5, 6];
+    let result = mean(numbers);
+    assert.equal(result, 3.5);
+  });
 
-  // Replace this with your real tests.
-  test('it works', function(assert) {
-    let result = statistics();
-    assert.ok(result);
+  test('modeString', function(assert) {
+    const strings = ['apple', 'pear', 'banana', 'apple'];
+    let result = modeString(strings);
+    assert.equal(result, 'apple');
   });
 });
