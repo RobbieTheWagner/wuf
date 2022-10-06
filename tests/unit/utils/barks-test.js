197fd6f87359299432dd9c5aa599c56ab346009f
Jared Galanis <jaredgalanis@users.noreply.github.com>
2022-10-05 23:41:29 -0400

    Test the bark utils

--- tests/unit/utils/barks-test.js
+++ tests/unit/utils/barks-test.js
@@ -4,22 +4,120 @@ import {
   determineBarkType,
   getTimeDomainMaxMin,
 } from 'wuf/utils/barks';
-import { module, skip, test } from 'qunit';
+import { module, test } from 'qunit';
+import convertDataURIToBinary from 'wuf/tests/helpers/convert-data-uri-to-binary';
+import base64Audio from 'wuf/tests/fixtures/sample-audio-data';
+import { waitUntil } from '@ember/test-helpers';
+import processDogRangeFrequency from 'wuf/tests/helpers/process-dog-range-frequency';
 
 module('Unit | Utility | barks', function () {
-  skip('determineBarkOccurred', function (assert) {
-    let result = determineBarkOccurred();
+  test('determineBarkOccurred', function (assert) {
+    let result = determineBarkOccurred([128, 190, 90, 111, 175]);
+
     assert.ok(result);
   });
 
-  skip('determineBarkPitch', function (assert) {
-    let result = determineBarkPitch();
-    assert.ok(result);
+  test('determineBarkPitch', async function (assert) {
+    const binary = convertDataURIToBinary(base64Audio);
+    const blob = new Blob([binary], { type: 'audio/webm' });
+
+    await processDogRangeFrequency.call(this, blob);
+
+    await waitUntil(() => this.dogRangeFrequencyArray);
+
+    debugger;
+
+    let result = determineBarkPitch(this.dogRangeFrequencyArray);
+
+    assert.equal(result, 'low');
   });
 
-  skip('determineBarkType', function (assert) {
-    let result = determineBarkType();
-    assert.ok(result);
+  test('determineBarkType', function (assert) {
+    const barksOccurred = [
+      false,
+      false,
+      false,
+      false,
+      false,
+      false,
+      false,
+      false,
+      false,
+      false,
+      false,
+      false,
+      false,
+      false,
+      true,
+      true,
+      false,
+      true,
+      true,
+      true,
+      true,
+      true,
+      true,
+      true,
+      true,
+      true,
+      true,
+      false,
+      false,
+      true,
+      false,
+      true,
+      true,
+      false,
+      true,
+      true,
+      false,
+      true,
+    ];
+
+    const pitches = [
+      'low',
+      'low',
+      'low',
+      'low',
+      'low',
+      'low',
+      'mid',
+      'mid',
+      'low',
+      'low',
+      'low',
+      'low',
+      'low',
+      'low',
+      'low',
+      'low',
+      'low',
+      'low',
+      'low',
+      'low',
+      'low',
+      'low',
+      'low',
+      'low',
+      'low',
+      'low',
+      'low',
+      'low',
+      'low',
+      'low',
+      'low',
+      'low',
+      'low',
+      'low',
+      'low',
+      'low',
+      'low',
+      'low',
+    ];
+
+    let result = determineBarkType(barksOccurred, pitches);
+
+    assert.equal(result, 'alert');
   });
 
   test('getTimeDomainMaxMin', function (assert) {
